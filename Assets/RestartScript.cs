c7bd204121427dd08f50ee7650e2478af9f252cf
gogomorgan <macrimorganb@gmail.com>
2018-05-04 11:27:02 -0400

    Updates from last time:
    -Replay button!
    -Difficulty spike keyed to progression: After the player's score reaches 10, platforms begin to both fall and spawn faster (minimum time between spawns is 0.4s, anything more was totally overwhelming). Yellow platforms also begin to spawn, giving players one more color to dodge, since their color will never match.

---
+++ Assets/RestartScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartScript : MonoBehaviour {
+
+    public void restart()
+    {
+        Debug.Log("restart!!!");
+        SceneManager.LoadScene(0);
+    }
+}
